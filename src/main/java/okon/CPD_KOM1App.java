a008ecb74fd17367d7d72581b998088fbd93bdb3
Grzegorz Okoń <grzegorz.okon@gmail.com>
2019-10-21 21:47:31 +0200

    Dodano funkcjonlność obslugi uruchamiania z parametrem w celu przekierowania wyniku do konsoli

--- src/main/java/okon/CPD_KOM1App.java
+++ src/main/java/okon/CPD_KOM1App.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Queue;
 
 public class CPD_KOM1App {
+    static boolean CONSOLE = false;
+    static boolean FILE = false;
     static final ConnectionFactory connectionFactory = new ConnectionFactory();
     static Queue<Job> jobQueue;
     static final List<Message> messageList = new ArrayList();
@@ -17,13 +19,34 @@ public class CPD_KOM1App {
     private final JobBuilder taskBuilder = new JobBuilder();
 
     public static void main (String args[]) {
+        setResultOutput(args);
         CPD_KOM1App cpd_kom1_app = new CPD_KOM1App();
-
         jobQueue = cpd_kom1_app.loadConfiguration("./settings/config.xml");
-
         cpd_kom1_app.startThreadPool(jobQueue.size());
+        cpd_kom1_app.print(messageList);
+    }
 
-        cpd_kom1_app.save(cpd_kom1_app.getJarFileName() + ".txt", messageList);
+    static void setResultOutput(String[] args) {
+        if (isConsoleOutput(args)) {
+            CONSOLE = true;
+        } else {
+            FILE = true;
+        }
+    }
+
+    static boolean isConsoleOutput(String[] args) {
+        if (args.length > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    static String getJarFileName() {
+        String path = CPD_KOM1App.class.getResource(CPD_KOM1App.class.getSimpleName() + ".class").getFile();
+        path = path.substring(0, path.lastIndexOf('!'));
+        path = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf('.'));
+
+        return path;
     }
 
     private Queue<Job> loadConfiguration(String pathname) {
@@ -53,36 +76,47 @@ public class CPD_KOM1App {
         }
     }
 
-    private String getJarFileName() {
-        String path = CPD_KOM1App.class.getResource(CPD_KOM1App.class.getSimpleName() + ".class").getFile();
-        path = path.substring(0, path.lastIndexOf('!'));
-        path = path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf('.'));
-
-        return path;
+    private void print(List<Message> content) {
+        if(CONSOLE) {
+            printToConsole(content);
+        }
+        if (FILE){
+            printToFile(content);
+        }
     }
 
-    public void save(String fileName, List<Message> content) {
-        try (FileOutputStream out = new FileOutputStream(new java.io.File(fileName))) {
-            for (Message message : content) {
-                MessageFormatter formatter = new MessageFormatter(message.getSizes());
+    private void printToConsole(List<Message> content) {
+        for(Message message : content) {
+            MessageFormatter formatter = new MessageFormatter(message.getSizes());
+            System.out.println(message.getHeader());
+            System.out.println();
+            System.out.println(formatter.format(message.getLabels()));
+            Arrays.fill(message.getLabels(), "---");
+            System.out.println(formatter.format(message.getLabels()));
+            for (String[] row : message.getRows()) {
+                System.out.println(formatter.format(row));
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
 
+    private void printToFile(List<Message> content) {
+        try (FileOutputStream out = new FileOutputStream(new java.io.File(CPD_KOM1App.getJarFileName() + ".txt"))) {
+            for(Message message : content) {
+                MessageFormatter formatter = new MessageFormatter(message.getSizes());
                 out.write(message.getHeader().getBytes());
                 out.write(System.getProperty("line.separator").getBytes());
                 out.write(System.getProperty("line.separator").getBytes());
-
                 out.write(formatter.format(message.getLabels()).getBytes());
                 out.write(System.getProperty("line.separator").getBytes());
-
                 Arrays.fill(message.getLabels(), "---");
-
                 out.write(formatter.format(message.getLabels()).getBytes());
                 out.write(System.getProperty("line.separator").getBytes());
-
                 for (String[] row : message.getRows()) {
                     out.write(formatter.format(row).getBytes());
                     out.write(System.getProperty("line.separator").getBytes());
                 }
-
                 out.write(System.getProperty("line.separator").getBytes());
                 out.write(System.getProperty("line.separator").getBytes());
             }
